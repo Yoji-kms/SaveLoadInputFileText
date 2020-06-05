9f8dcb812e3e111b0620f82d9e76d9331fdfe8fe
Mashnin Egor <y0g@mail.ru>
2020-06-05 18:09:31 +1000

    Login without saved user bug fixed

--- app/src/main/java/com/yoji/saveloadinputfiletext/MainActivity.java
+++ app/src/main/java/com/yoji/saveloadinputfiletext/MainActivity.java
@@ -13,7 +13,6 @@ import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
